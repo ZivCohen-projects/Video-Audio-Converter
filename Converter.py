7c4f086b5e7b8dff42ced77d12a1175e77119512
Ziv Cohen <zcohen@ku.edu>
2025-08-27 23:02:57 -0500

    Added main file

--- Converter.py
+++ Converter.py
@@ -1,5 +1,4 @@
 import subprocess
-import os
 
 def convert_video_to_mp3(input_file, output_file):
     ffmpeg_cmd = [
@@ -18,4 +17,12 @@ def convert_video_to_mp3(input_file, output_file):
     except subprocess.CalledProcessError as e:
         print("Conversion failed")
         
+def main():
+    filename = input("What is the name of your file?")
+    if filename.endswith(".mov") or filename.endswith(".mp4"):
+        convert_video_to_mp3(filename, f"audio_{filename}.mp3")
+    else:
+        print("File is not audio")
+        
+main()
 
